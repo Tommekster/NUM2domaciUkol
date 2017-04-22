ad3e0f174e4ef01459439d5cc2a8549687382191
Tomas Zikmund <zikmuto2@fjfi.cvut.cz>
2017-04-22 15:33:50 +0200

    Rungoeva-Kuttova metoda

---
+++ ode/hw02RungeKutta.h
@@ -0,0 +1,83 @@
+/* 
+ * File:   hw02RungeKutta.h
+ * Author: zikmuto2
+ *
+ * Created on April 21, 2017, 4:46 PM
+ */
+
+#ifndef RUNGEKUTTA_H
+#define	RUNGEKUTTA_H
+
+template< typename Problem >
+class RungeKutt : public IntegratorBase
+{
+   public:
+      
+      RungeKutt( Problem& problem )
+      {
+         this->k1 = new double[ problem.getDegreesOfFreedom() ];
+         this->k2 = new double[ problem.getDegreesOfFreedom() ];
+         this->aux = new double[ problem.getDegreesOfFreedom() ];
+      }
+
+      bool solve( Problem& problem,
+                  double* u )
+      {
+         const int dofs = problem.getDegreesOfFreedom();
+         double tau = std::min( this->integrationTimeStep, this->stopTime - this->time ); // rekl bych, zo toto znacime 'h'
+         long int iteration( 0 );
+         while( this->time < this->stopTime )
+         {
+            /****
+             * Compute k1
+             */
+            // k1 = f(t,u)
+            problem.getRightHandSide( this->time/* t */, u /* u */, k1 /* result */ ); 
+            
+            /****
+             * Compute k2
+             */
+            // aux = u + tau/3*k1
+            for( int i = 0; i < dofs; i++ )
+               aux[ i ] = u[ i ] + tau * ( 1.0 / 3.0 * k1[ i ] );
+
+            // k2 = f(t + tau/3, u+tau/3*k1)
+            problem.getRightHandSide( this->time + 1.0 / 3.0 * tau, aux, k2 );
+            
+            /***
+             * vysledek: \delta u = tau * ( p1*k1(tau) + p2*k2(tau) )
+             * p1 = p2 = 1/2
+             */
+             for( int i = 0; i < dofs; i++ )
+                u[ i ] += tau / 5.0 * ( k1[ i ] + k2[ i ] );
+             this->time += tau;
+             iteration++;
+             if( iteration > 100000 )
+             {
+                std::cerr << "The solver has reached the maximum number of iteratoins. " << std::endl;
+                return false;
+             }
+            //if( this->adaptivity )
+            //   tau *= 0.8 * pow( this->adaptivity / eps, 0.2 );
+            tau = std::min( tau, this->stopTime - this->time );
+            std::cout << "ITER: " << iteration << " \t tau = " << tau << " \t time= " << time << "         \r " << std::flush;
+         }
+         std::cout << std::endl;
+         return true;
+      }
+      
+      ~Merson()
+      {
+         delete[] k1;
+         delete[] k2;
+         delete[] aux;
+      }
+      
+   protected:
+      
+      double *k1, *k2, *aux;
+      
+};
+
+#endif	/* RUNGEKUTTA_H */
+
